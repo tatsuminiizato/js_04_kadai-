24975e1c014593e342e49995af54dd393cdb0681
tatsuminiizato <80142110+tatsuminiizato@users.noreply.github.com>
2021-03-13 17:52:13 +0900

    Add files via upload

---
+++ campphp/index.php
@@ -0,0 +1,78 @@
+
+
+
+<?php
+
+echo 'おみくじ';
+
+echo '<br>';
+
+$omikuzi = mt_rand (1,5);
+                   
+if($omikuzi==1){
+    echo ' <img src="img/daikichi.jpeg" alt="">';
+
+}
+
+if($omikuzi==2){
+    echo '<img src="img/chukichi.png" alt="">';
+
+}
+
+if($omikuzi==3){
+    echo '<img src="img/shokichi.png" alt="">';
+
+
+}
+
+if($omikuzi==4){
+    echo '<img src="img/suekichi.png" alt="">';
+
+}
+
+if($omikuzi==5){
+    echo '<img src="img/kyo.png" alt="">';
+
+}
+
+?>
+
+
+<br>
+
+<?php
+
+echo 'おみくじ';
+
+echo '<br>';
+
+$omikuzi = mt_rand (1,5);
+                   
+if($omikuzi==1){
+    echo ' <img src="img/daikichi.jpeg" alt="">';
+
+}
+
+if($omikuzi==2){
+    echo '<img src="img/chukichi.png" alt="">';
+
+}
+
+if($omikuzi==3){
+    echo '<img src="img/shokichi.png" alt="">';
+
+
+}
+
+if($omikuzi==4){
+    echo '<img src="img/suekichi.png" alt="">';
+
+}
+
+if($omikuzi==5){
+    echo '<img src="img/kyo.png" alt="">';
+
+}
+
+?>
+
